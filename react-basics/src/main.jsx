7616776a802f2d6a9cd81123c30bfaea7d1c0ebd
Sadık Turan <sadikturangmail.com>
2024-09-23 13:28:25 +0300

    2_7_projemizi_inceleyelim

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+function App() {
+  return <h1>Product List</h1>;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
