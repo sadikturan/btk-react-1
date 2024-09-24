1766e3bedb73f869cd4c7acb5904b841454ca16a
Sadık Turan <sadikturangmail.com>
2024-09-24 13:32:33 +0300

    3_4_component_props

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -15,37 +15,60 @@ function Header() {
 }
 
 function ProducList() {
+  const items = [
+    {
+      "image" :  "1.jpg",
+      "title" : "IPhone 16",
+      "description" : "Lorem ipsum dolor sit amet.",
+      "price": 80000
+    },
+    {
+      "image" :  "2.jpg",
+      "title" : "IPhone 17",
+      "description" : "Lorem ipsum dolor sit amet.",
+      "price": 90000
+    },
+    {
+      "image" :  "3.jpg",
+      "title" : "IPhone 18",
+      "description" : "Lorem ipsum dolor sit amet.",
+      "price": 100000
+    }
+  ];
+
   return (
     <>
       <h2>Product List</h2>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product 
+          image={ items[0].image } 
+          title={items[0].title} 
+          description={items[0].description} 
+          price={items[0].price}/>
+
+      <Product 
+          image={ items[1].image } 
+          title={items[1].title} 
+          description={items[1].description} 
+          price={items[1].price}/>
+
+      <Product 
+          image={ items[2].image } 
+          title={items[2].title} 
+          description={items[2].description} 
+          price={items[2].price}/>
+            
     </>
   );  
 }
 
-function Product() {
-  const image = "1.jpg";
-  const title = "IPhone 16";
-  const description = "Lorem ipsum dolor sit amet.";
-  const price = 80000;
-
-  const product_item = {
-    "image" :  "1.jpg",
-    "title" : "IPhone 16",
-    "description" : "Lorem ipsum dolor sit amet.",
-    "price": 80000
-  };
-
+function Product(props) {
+  console.log(props);
   return (
     <div>
-      <img src={ "/img/" + product_item.image } alt="" />
-      <h2>{ product_item.title }</h2>
-      <p>{ product_item.description }</p>
-      <span>{ product_item.price } TL</span>
+      <img src={ "/img/" + props.image } alt="" />
+      <h2>{ props.title }</h2>
+      <p>{ props.description }</p>
+      <span>{ props.price } TL</span>
     </div>
   );
 }
