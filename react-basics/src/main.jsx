48d46db5e2e722818444fa9278f17d9211873db0
Sadık Turan <sadikturangmail.com>
2024-09-24 11:29:54 +0300

    3_2_component_ekleme

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -2,7 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 function App() {
-  return <h1>Product List</h1>;
+  return (
+    <>
+      <Header />
+      <ProducList />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Header</h1>;
+}
+
+function ProducList() {
+  return (
+    <>
+      <h2>Product List</h2>
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );  
+}
+
+function Product() {
+  return <h3>Product</h3>;
 }
 
 createRoot(document.getElementById('root')).render(
