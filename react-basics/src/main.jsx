c8316794bf4f0afc4de5b44675b85c58eaa40bab
Sadık Turan <sadikturangmail.com>
2024-09-24 12:17:02 +0300

    3_3_jsx_expressions

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -21,12 +21,33 @@ function ProducList() {
       <Product />
       <Product />
       <Product />
+      <Product />
+      <Product />
     </>
   );  
 }
 
 function Product() {
-  return <h3>Product</h3>;
+  const image = "1.jpg";
+  const title = "IPhone 16";
+  const description = "Lorem ipsum dolor sit amet.";
+  const price = 80000;
+
+  const product_item = {
+    "image" :  "1.jpg",
+    "title" : "IPhone 16",
+    "description" : "Lorem ipsum dolor sit amet.",
+    "price": 80000
+  };
+
+  return (
+    <div>
+      <img src={ "/img/" + product_item.image } alt="" />
+      <h2>{ product_item.title }</h2>
+      <p>{ product_item.description }</p>
+      <span>{ product_item.price } TL</span>
+    </div>
+  );
 }
 
 createRoot(document.getElementById('root')).render(
