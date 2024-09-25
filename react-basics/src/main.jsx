eb9312b751c2ed07d9dfda3d4a7e338bdb88592c
Sadık Turan <sadikturangmail.com>
2024-09-25 09:32:13 +0300

    3_6_jsx_kosullu_durumlar

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <Header />
       <ProducList />
+      <Footer />
     </>
   );
 }
@@ -20,42 +21,59 @@ function ProducList() {
       "image" :  "1.jpg",
       "title" : "IPhone 16",
       "description" : "Lorem ipsum dolor sit amet.",
-      "price": 80000
+      "price": 80000,
+      "is_active": true
     },
     {
       "image" :  "2.jpg",
       "title" : "IPhone 17",
       "description" : "Lorem ipsum dolor sit amet.",
-      "price": 90000
+      "price": 90000,
+      "is_active": true
     },
     {
       "image" :  "3.jpg",
       "title" : "IPhone 18",
       "description" : "Lorem ipsum dolor sit amet.",
-      "price": 100000
+      "price": 100000,
+      "is_active": true
     }
     ,
     {
       "image" :  "4.jpg",
       "title" : "IPhone 19",
       "description" : "Lorem ipsum dolor sit amet.",
-      "price": 110000
+      "price": 110000,
+      "is_active": true
     }
   ];
+
+  // const items = [];
 
   return (
     <>
       <h2>Product List</h2>
       {
-        items.map((item, index) => (
-          <Product key={ index } productObj = { item } />
-        ))
-      }  
+        items.length > 0 ? (
+          <div id='product-list'>
+          {
+            items.map((item, index) => (
+              <Product key={ index } productObj = { item } />
+            ))
+          }  
+        </div>
+        ) : (
+          <p>Şu anda satışta olan ürünümüz yok.</p>
+        )
+      }
     </>
   );  
 }
 
 function Product({ productObj }) {
+
+  if(!productObj.is_active) return null;
+
   return (
     <div>
       <img src={ "/img/" + productObj.image } alt="" />
@@ -63,6 +81,26 @@ function Product({ productObj }) {
       <p>{ productObj.description }</p>
       <span>{ productObj.price } TL</span>
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 20;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer>
+      {
+        isOpen ? (
+          <p>Akşam {closeHour}' e kadar sipariş verebilirsiniz.</p>
+        ) : 
+        (
+          <p>Şu an kapalıyız. Açılış saatimiz {openHour}.</p>
+        )
+      }
+    </footer>
   );
 }
 
