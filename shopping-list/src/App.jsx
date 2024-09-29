863be3643144567b804a1875edb3371a709d891c
Sadık Turan <sadikturangmail.com>
2024-09-29 22:35:13 +0300

    final project

---
+++ shopping-list/src/App.jsx
@@ -0,0 +1,59 @@
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <List />
+      <Summary />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <h1>🛒 Shopping List</h1>
+  );
+}
+
+function Form() {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Ürün adı giriniz" />
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <button type="submit">Ekle</button>
+    </form>
+  );
+}
+
+function List() {
+  return (
+    <div className="list">
+      <ul>
+        <Item />
+        <Item />
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <li>
+      <span>Yumurta</span>
+      <button>X</button>
+    </li>
+  );
+}
+
+function Summary() {
+  return (
+    <footer className="summary">Alışveriş sepetinizde 10 ürün bulunmaktadır.</footer>
+  );
+}
+
+export default App
