05f39169cee6584c66086112d92985c088d6b01c
Sadık Turan <sadikturangmail.com>
2024-09-30 11:58:33 +0300

    4_6_form_data

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const items = [
   { id:1, title: "Yumurta", quantity: 10, completed: true },
   { id:1, title: "Ekmek", quantity: 2, completed: true },
@@ -24,15 +26,23 @@ function Header() {
 }
 
 function Form() {
+  const [title, setTitle] = useState("");
+  const [quantity, setQuantity] = useState(1)
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const item = {id: Date.now(), title, quantity, completed: false};
+    console.log(item);
+
+    setTitle('');
+    setQuantity(1);
+  }
+  
   return (
-    <form className="form">
-      <input type="text" placeholder="Ürün adı giriniz" />
-      <select>
-        {
-          Array.from({length:10}, (v,i) => i + 1)
-            .map(num => <option value={num}>{num}</option>)
-        }
-       
+    <form className="form" onSubmit={handleFormSubmit}>
+      <input type="text" placeholder="Ürün adı giriniz" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        { Array.from({length:10}, (v,i) => i + 1).map(num => <option value={num} key={num}>{num}</option>) }
       </select>
       <button type="submit">Ekle</button>
     </form>
