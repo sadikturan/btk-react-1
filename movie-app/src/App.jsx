cde7cbd4d705e8d34da2bffad8694526ce8a09e5
Sadık Turan <sadikturangmail.com>
2024-10-21 11:55:09 +0300

    6_3_async_await

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,83 +1,25 @@
 import { useEffect, useState } from "react";
 
-const movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://image.tmdb.org/t/p/original/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
-  },
-];
-
-const selected_movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-    duration: 120,
-    rating: 8.4,
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-    duration: 125,
-    rating: 8.8,
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
-    duration: 125,
-    rating: 8.8,
-  },
-];
-
 const getAverage = (array) =>
   array.reduce((sum, value) => sum + value, 2) / array.length;
 
-const api_key = "<api_key>";
-const query = "fight";
+const api_key = "e8a9d5f82d78ca210c8076d648685c12";
+const query = "father";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [selectedMovies, setSelectedMovies] = useState([]);
 
   useEffect(function () {
-    // First Render (Mount)
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovies(data.results));
+    async function getMovies() {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.results);
+    }
+    console.log(movies);
+    getMovies();
   }, []);
 
   return (
@@ -170,7 +112,7 @@ function MovieList({ movies }) {
   return (
     <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
       {movies.map((movie) => (
-        <Movie movie={movie} key={movie.Id} />
+        <Movie movie={movie} key={movie.id} />
       ))}
     </div>
   );
@@ -203,8 +145,8 @@ function Movie({ movie }) {
 }
 
 function MyListSummary({ selectedMovies }) {
-  const avgRating = getAverage(selected_movie_list.map((m) => m.rating));
-  const avgDuration = getAverage(selected_movie_list.map((m) => m.duration));
+  const avgRating = getAverage(selectedMovies.map((m) => m.rating));
+  const avgDuration = getAverage(selectedMovies.map((m) => m.duration));
   return (
     <div className="card mb-2">
       <div className="card-body">
