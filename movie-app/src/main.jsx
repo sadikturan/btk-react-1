9b35f66ba2d16fa3664db272783471c0c1068cc4
Sadık Turan <sadikturangmail.com>
2024-10-27 22:33:47 +0300

    6_16_component_files

--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -3,13 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import StarRating from "./StarRating.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* <StarRating maxRating={5} color="red" />
-    <StarRating maxRating={10} />
-    <StarRating maxRating={5} size={80} /> */}
   </StrictMode>
 );
