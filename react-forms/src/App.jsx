176af8af1c11108a5b51bfd8779931f5dbbf44c0
Sadık Turan <sadikturangmail.com>
2024-10-28 21:47:15 +0300

    7_5_user_inputs_formdata

--- react-forms/src/App.jsx
+++ react-forms/src/App.jsx
@@ -1,9 +1,9 @@
-import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="container my-4">
-      <Login />
+      <Register />
     </div>
   );
 }
