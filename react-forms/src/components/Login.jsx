ae99a02dc463f29b8530d077869b4d01bd8afa94
Sadık Turan <sadikturangmail.com>
2024-10-30 13:12:15 +0300

    8_3_validation_on_submit

--- react-forms/src/components/Login.jsx
+++ react-forms/src/components/Login.jsx
@@ -1,21 +1,45 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    console.log(email.current.value);
-    console.log(password.current.value);
+    setEmailError(false);
+    setPasswordError(false);
+
+    const emailVal = email.current.value;
+    const passwordVal = password.current.value;
+
+    const emailIsInvalid = !emailVal.includes("@");
+    const passwordIsInvalid = passwordVal.length <= 5;
+
+    if (emailIsInvalid) {
+      setEmailError(true);
+      return;
+    }
+
+    if (passwordIsInvalid) {
+      setPasswordError(true);
+      return;
+    }
+
+    console.log("form submitted");
+
+    setEmailError(false);
+    setPasswordError(false);
 
     email.current.value = "";
     password.current.value = "";
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
       <div className="header">
         <h1>Login</h1>
         <p>Please enter your login and password!</p>
@@ -32,6 +56,9 @@ export default function Login() {
           name="email"
           ref={email}
         />
+        {emailError && (
+          <div className="invalid-feedback d-block">Enter valid email.</div>
+        )}
       </div>
 
       <div className="mb-4">
@@ -44,7 +71,12 @@ export default function Login() {
           id="password"
           name="password"
           ref={password}
-        />
+        />{" "}
+        {passwordError && (
+          <div className="invalid-feedback d-block">
+            Parola min. 5 karakter olmalıdır.
+          </div>
+        )}
       </div>
 
       <div className="mb-3">
