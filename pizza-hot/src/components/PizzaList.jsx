569936c6ce192c1c0d12136051aa48db701c1683
Sadık Turan <sadikturangmail.com>
2024-11-01 23:26:07 +0300

    9_2_pizza_listesi_http_get_request

--- pizza-hot/src/components/PizzaList.jsx
+++ pizza-hot/src/components/PizzaList.jsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from "react";
 import Pizza from "./Pizza";
 
 export default function PizzaList() {
+  const [loadedPizzas, setLoadedPizzas] = useState([]);
+
+  useEffect(() => {
+    async function getPizzaList() {
+      const response = await fetch("http://localhost:3000/pizzas");
+
+      if (!response.ok) {
+        // error log
+      }
+
+      const pizzas = await response.json();
+      setLoadedPizzas(pizzas);
+    }
+
+    getPizzaList();
+  }, []);
+
   return (
     <div className="pizza-list">
       <div className="row row-cols-2 row-cols-md-3 row-cols-xl-4 g-4">
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
+        {loadedPizzas.map((pizza) => (
+          <Pizza />
+        ))}
       </div>
     </div>
   );
