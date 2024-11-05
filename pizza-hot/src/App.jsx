568a391c3c1ca79d21d87b939ca73c3e7ef5311e
Sadık Turan <sadikturangmail.com>
2024-11-05 14:34:41 +0300

    9_11_reusable_modal_component

--- pizza-hot/src/App.jsx
+++ pizza-hot/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import PizzaList from "./components/PizzaList";
 import ThemeSelector from "./components/ThemeSelector";
 import { ThemeContext, ThemeProvider } from "./contexts/ThemeContext";
+import Cart from "./components/Cart";
 
 export default function App() {
   const { mode } = useContext(ThemeContext);
@@ -15,6 +16,7 @@ export default function App() {
       <ThemeSelector />
       <div className="container my-4">
         <PizzaList />
+        <Cart />
       </div>
     </div>
   );
