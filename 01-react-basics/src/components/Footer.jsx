5d3578447b7a1c77ffed8ab7eb0364fb3cfeb139
Sadık Turan <sadikturangmail.com>
2024-11-18 11:03:33 +0300

    completed projects

---
+++ 01-react-basics/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 8;
+    const closeHour = 20;
+    const isOpen = hour >= openHour && hour <= closeHour;
+  
+    return (
+      <footer>
+        {
+          isOpen ? (
+            <p>Akşam {closeHour}' e kadar sipariş verebilirsiniz.</p>
+          ) : 
+          (
+            <p>Şu an kapalıyız. Açılış saatimiz {openHour}.</p>
+          )
+        }
+      </footer>
+    );
+  }
